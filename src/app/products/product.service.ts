49026efe354817c4e082b0ac090a450586bfdb91
ldex <laurent.duveau+git@gmail.com>
2022-10-18 13:01:31 -0400

    Fin du workshop

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError, delay, shareReplay, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, delay, filter, first, map, mergeAll, shareReplay, switchMap, tap } from 'rxjs/operators';
 import { Product } from './product.interface';
 
 @Injectable({
@@ -10,23 +10,64 @@ import { Product } from './product.interface';
 export class ProductService {
 
   private baseUrl = 'https://storerestservice.azurewebsites.net/api/products/';
-  products$: Observable<Product[]>;
+  private productsSubject = new BehaviorSubject<Product[]>([]);
+  products$: Observable<Product[]> = this.productsSubject.asObservable();
+  mostExpensiveProduct$: Observable<Product>;
+  productsToLoad = 10;
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
+    this.initProducts();
+    this.initMostExpensiveProduct();
+  }
+
+  resetList(): void {
+    this.productsSubject.next([]);
     this.initProducts();
   }
 
-  initProducts() {
-    let url:string = this.baseUrl + `?$orderby=ModifiedDate%20desc`;
-
-    this.products$ = this
-                      .http
-                      .get<Product[]>(url)
+  private initMostExpensiveProduct(): void {
+    this.mostExpensiveProduct$ =
+      this
+      .products$
+      .pipe(
+        filter(products => products.length > 0),
+        switchMap(
+          products => of(products)
                       .pipe(
-                        delay(1500),
-                        tap(console.table),
-                        shareReplay()
-                      );
+                        map(products => [...products].sort((p1, p2) => p1.price > p2.price ? -1 : 1)),
+                        // [{}, {}, {}]
+                        mergeAll(),
+                        // {}, {}, {}
+                        first()
+                      )
+        )
+      )
+  }
+
+  // loadProducts(): Observable<Product[]> {
+  //   return this.http.get<Product[]>(this.baseUrl);
+  // }
+
+  initProducts(skip = 0, take = this.productsToLoad): void {
+    let url = this.baseUrl + `?$skip=${skip}&$top=${take}&$orderby=ModifiedDate%20desc`;
+
+    this
+      .http
+      .get<Product[]>(url)
+      .pipe(
+        delay(1500), // pour la demo...
+        tap(data => console.table(data)),
+        shareReplay(),
+        map(
+          newProducts => {
+            let currentProduts = this.productsSubject.value;
+            return currentProduts.concat(newProducts);
+          }
+        )
+      )
+      .subscribe(
+        fullProductList => this.productsSubject.next(fullProductList)
+      );
   }
 
   insertProduct(newProduct: Product): Observable<Product> {
@@ -34,6 +75,6 @@ export class ProductService {
   }
 
   deleteProduct(id: number): Observable<any> {
-    return this.http.delete(this.baseUrl + id);           
+    return this.http.delete(this.baseUrl + id);
   }
 }
